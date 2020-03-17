4c6edbd4d9c8d74023c96dec83b2f6d5a1559609
Olia1011 <olgamelnyk10111989@gmail.com>
2020-03-17 21:10:42 +0200

    first commit

---
+++ js/task-4.js
@@ -0,0 +1,42 @@
+'use strict'
+
+class StringBuilder {
+
+    constructor(string) {
+        this._value = string;
+    }
+
+    get value() {
+        return this._value;
+    }
+
+    append(string) {
+         this._value = this._value + string;
+        return string;
+    }
+
+    prepend(string) {
+      this._value = string + this._value;
+      return string;
+    }
+
+    pad(string) {
+        this.prepend(string);
+        this.append(string);
+        return string;
+    }
+
+}
+
+const builder = new StringBuilder(".");
+
+builder.append("^");
+console.log(builder.value); // '.^'
+
+builder.prepend("^");
+console.log(builder.value); // '^.^'
+
+builder.pad("=");
+console.log(builder.value); // '=^.^='
+
+
---
+++ js/task-5.js
@@ -0,0 +1,73 @@
+"use strict";
+
+class Car {
+  static getSpecs(car) {
+    console.log(
+      `maxSpeed: ${car.maxSpeed}, speed: ${car.speed}, isOn ${car.isOn}, distance ${car.distance}, price ${car.price}`
+    );
+  }
+
+  constructor(car) {
+    this.speed = 0;
+    this.price = car.price;
+    this.maxSpeed = car.maxSpeed;
+    this.isOn = false;
+    this.distance = 0;
+  }
+
+  get carPrice() {
+    return this.price;
+  }
+
+  set carPrice(price) {
+    this.price = price;
+  }
+
+  turnOn() {
+    this.isOn = true;
+  }
+
+  turnOff() {
+    this.isOn = false;
+  }
+
+  accelerate(value) {
+    let newSpeed = this.speed + value;
+    if (newSpeed < this.maxSpeed) {
+      this.speed = newSpeed;
+    }
+  }
+
+  decelerate(value) {
+    let newSpeed = this.speed - value;
+    if (newSpeed > 0) {
+      this.speed = newSpeed;
+    }
+  }
+
+  drive(hours) {
+    if (this.turnOn) {
+      this.distance += hours * this.speed;
+    }
+  }
+}
+
+const mustang = new Car({ maxSpeed: 200, price: 2000 });
+
+mustang.turnOn();
+mustang.accelerate(50);
+mustang.drive(2);
+
+Car.getSpecs(mustang);
+// maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
+
+mustang.decelerate(20);
+mustang.drive(1);
+mustang.turnOff();
+
+Car.getSpecs(mustang);
+// maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000
+
+console.log(mustang.price); // 2000
+mustang.price = 4000;
+console.log(mustang.price); // 4000
